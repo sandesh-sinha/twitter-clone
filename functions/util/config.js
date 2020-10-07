cc657aad12f60144cf0bacdc878159e8c0ae6578
Sandesh-Sinha <sandesh85210@gmail.com>
2020-10-08 04:03:43 +0530

    backend complete

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyCCNMj3umAeDqRAwh9E30eoI_MBLFUVg0w",
+    authDomain: "twitter-clone-a4e66.firebaseapp.com",
+    databaseURL: "https://twitter-clone-a4e66.firebaseio.com",
+    projectId: "twitter-clone-a4e66",
+    storageBucket: "twitter-clone-a4e66.appspot.com",
+    messagingSenderId: "476796912260",
+    appId: "1:476796912260:web:0a95484c51acd5219a38d1"
+  };
